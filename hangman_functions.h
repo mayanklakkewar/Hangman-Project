4cbcfdc8bb0819f8a82490a123512c1666314a92
Mayank Satish Lakkewar <97389735+mayanklakkewar@users.noreply.github.com>
2024-08-05 17:44:27 +0530

    Add files via upload

---
+++ hangman_functions.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void greet();
+void display_misses(int misses);
+void display_status(vector<char> incorrect, string answer);
+void end_game(string answer,string codeword);
